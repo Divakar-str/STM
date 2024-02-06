e4a506f579e7a5589f93e7de5f58048ec118f932
DHIWAKAR N <103329655+Divakar-str@users.noreply.github.com>
2024-02-06 14:03:40 +0530

    Update fullno.js

--- js/fullno.js
+++ js/fullno.js
@@ -156,5 +156,5 @@ window.addEventListener('load', function() {
     setTimeout(function() {
       var loader = document.getElementById('loader');
       loader.style.display = 'none';
-    }, 3000); // Replace 3000 with the desired timeout value in milliseconds
+    }, 1000); // Replace 3000 with the desired timeout value in milliseconds
   });
